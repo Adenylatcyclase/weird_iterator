e0f0c4108e1cc74852db997fe468c1752c3b7e67
Adenylatcyclase <streicher1993@googlemail.com>
2021-03-04 13:24:07 +0100

    clippy + fmt

--- src/spiral.rs
+++ src/spiral.rs
@@ -1,26 +1,26 @@
-pub trait ToSpiral<F, T: Iterator<Item=F>, I: Iterator<Item=T>> {
+pub trait ToSpiral<F, T: Iterator<Item = F>, I: Iterator<Item = T>> {
     fn spiral(self) -> Spiral<F, T, I>;
 }
 
-pub struct Spiral<F, T: Iterator<Item=F>, I: Iterator<Item=T>> {
+pub struct Spiral<F, T: Iterator<Item = F>, I: Iterator<Item = T>> {
     iter: I,
     subiter: Vec<T>,
     index: usize,
     fin: bool,
 }
 
-impl<F, T: Iterator<Item=F>, I: Iterator<Item = T>> ToSpiral<F, T, I> for I {
+impl<F, T: Iterator<Item = F>, I: Iterator<Item = T>> ToSpiral<F, T, I> for I {
     fn spiral(self) -> Spiral<F, T, I> {
         Spiral {
             iter: self,
-            subiter: vec!(),
+            subiter: vec![],
             index: 0,
             fin: false,
         }
     }
 }
 
-impl<F, T: Iterator<Item=F>, I: Iterator<Item=T>> Spiral<F, T, I> {
+impl<F, T: Iterator<Item = F>, I: Iterator<Item = T>> Spiral<F, T, I> {
     fn step(&mut self) {
         self.index += 1;
         if self.index >= self.subiter.len() {
@@ -28,15 +28,15 @@ impl<F, T: Iterator<Item=F>, I: Iterator<Item=T>> Spiral<F, T, I> {
         }
     }
 }
-impl<F, T: Iterator<Item=F>, I: Iterator<Item = T>> Iterator for Spiral<F, T, I> {
+impl<F, T: Iterator<Item = F>, I: Iterator<Item = T>> Iterator for Spiral<F, T, I> {
     type Item = F;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let (Some(iter), false) =  (self.iter.next(), self.fin) {
+        if let (Some(iter), false) = (self.iter.next(), self.fin) {
             self.subiter.push(iter);
             self.index = self.subiter.len() - 1;
         }
-        if self.subiter.len() == 0 {
+        if self.subiter.is_empty() {
             return None;
         }
         let i = self.index;
@@ -59,6 +59,13 @@ mod tests {
 
     #[test]
     fn spiral_test() {
-        assert_eq!("abc\ndef\nghi".lines().map(|s| s.chars()).spiral().collect::<String>(), "adgbehcfi".to_owned());
+        assert_eq!(
+            "abc\ndef\nghi"
+                .lines()
+                .map(|s| s.chars())
+                .spiral()
+                .collect::<String>(),
+            "adgbehcfi".to_owned()
+        );
     }
 }
